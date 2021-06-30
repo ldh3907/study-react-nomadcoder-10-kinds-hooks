df78b4cc8efdabb85c24046660e5fb8f732f9bea
ldh3907 <ldh165163@gmail.com>
2021-06-30 14:31:47 +0900

    add : count file
    add : count file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Count from "./components/count";
 
 function App() {
-  return;
+  return (
+    <HashRouter>
+      <Route path="/" exact={ture} component={Count} />
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/components/count.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const count = () => {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <section id="count-container">
+      <h1>{item}</h1>
+      <button type="button" onClick={incrementItem}>
+        +1
+      </button>
+      <button type="button" onClick={decrementItem}>
+        -1
+      </button>
+      <h2>Current Number</h2>
+    </section>
+  );
+};
+
+export default count;
