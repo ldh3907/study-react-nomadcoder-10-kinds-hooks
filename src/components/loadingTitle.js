959381b5c479c8316a26588fe1d2d0f5abb4aefe
ldh3907 <ldh165163@gmail.com>
2021-07-05 19:39:23 +0900

    update : loadingtitle
    update : loadingtitle

--- src/components/loadingTitle.js
+++ src/components/loadingTitle.js
@@ -12,9 +12,22 @@ const LoadingTitle = () => {
     return setTitle;
   };
   const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("10 Hooks"), 5000);
   return (
     <section id="LoadingTitle">
-      <div className="loadingtitle-container"></div>
+      <div className="loadingtitle-container">
+        <div className="loadingtitle-wrap">
+          <div className="loadingtitle-title">Loading Title</div>
+          <div className="loadingtitle-content-wrap">
+            <div className="loadingtitle-express">
+              새로고침을 누르면 타이틀이 로딩됩니다.
+            </div>
+            <div className="loadingtitle-explain">
+              useEffect 훅을 사용하여 만들었습니다.
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
