9dc3ca20a6c041f3e2bf993a4199e59d5035e769
ldh3907 <ldh165163@gmail.com>
2021-07-06 15:41:50 +0900

    update: confirminput
    update: confirminput

--- src/components/confirminput.js
+++ src/components/confirminput.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import "./confirminput.css";
 
 const ConfirmInput = () => {
+  const [alret, setAlret] = useState(" ");
   const useConfirm = (message = "", callback, rejection) => {
     if (typeof callback !== "function") {
       return;
@@ -15,8 +16,14 @@ const ConfirmInput = () => {
     };
     return confirmAction;
   };
-  const deleteWorld = () => console.log("연결을 해제합니다");
-  const abort = () => console.log("연결을 유지합니다");
+  const deleteWorld = () => {
+    console.log("연결을 해제합니다.");
+    setAlret("연결을 해제합니다");
+  };
+  const abort = () => {
+    console.log("연결을 유지합니다.");
+    setAlret("연결을 유지합니다");
+  };
   const confrimDelete = useConfirm(
     "정말 해제 하시겠습니까?",
     deleteWorld,
@@ -25,7 +32,19 @@ const ConfirmInput = () => {
   return (
     <section id="confirminput">
       <div className="confirminput-container">
-        <button onClick={confrimDelete}>Delete the world</button>
+        <div className="confirminput-wrap">
+          <div className="confirminput-title">Confirm Input</div>
+          <div className="confirminput-content-wrap">
+            <button className="confirminput-button" onClick={confrimDelete}>
+              연결끊기
+            </button>
+            <div className="confirminput-alert">{alret}</div>
+            <div className="confirminput-express">버튼을 눌러주세요</div>
+            <div className="confirminput-explain">
+              useState, useEffect 훅을 사용하여 만들었습니다.
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
