37305f72599b9063bcdbcf823842a4d65570a40a
ldh3907 <ldh165163@gmail.com>
2021-07-07 09:43:34 +0900

    update : confirmexit
    update : confirmexit

--- src/components/confirmexit.js
+++ src/components/confirmexit.js
@@ -35,6 +35,10 @@ const ConfirmExit = () => {
               </button>
             </div>
             <div className="confirmexit-alert">{error}</div>
+            <div className="confirmexit-express">버튼을 눌러주세요</div>
+            <div className="confrimexit-explain">
+              useState, usePreventLeave 훅을 사용하여 만들었습니다.
+            </div>
           </div>
         </div>
       </div>
