07a814cf1933e3018376edd46c94c645af39d66a
ldh3907 <ldh165163@gmail.com>
2021-07-08 12:13:31 +0900

    update : chknetwork
    update : chknetwork

--- src/components/chknetwork.js
+++ src/components/chknetwork.js
@@ -27,8 +27,19 @@ const CheckNetwork = () => {
   return (
     <section id="chknetwork">
       <div className="chknetwork-container">
-        <div className="chknetwork-alert">
-          {onLine ? "온라인 상태입니다" : "오프라인 상태입니다"}
+        <div className="chknetwork-wrap">
+          <div className="chknetwork-title">Check Network</div>
+          <div className="chknetwork-content-wrap">
+            <div className="chknetwork-alert">
+              {onLine ? "온라인 상태입니다" : "오프라인 상태입니다"}
+            </div>
+            <div class="chknetwork-process">
+              와이파이를 해제 하고 새로고침을 해보세요
+            </div>
+            <div className="chknetwork-explain">
+              useState, useEffect, useNetwork 훅을 사용하여 만들었습니다.
+            </div>
+          </div>
         </div>
       </div>
     </section>
