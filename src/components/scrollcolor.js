5ba8adbe2d74425ab29b7b20007c27f81326760b
ldh3907 <ldh165163@gmail.com>
2021-07-08 14:38:40 +0900

    update : scrollcolor
    update : scrollcolor

--- src/components/scrollcolor.js
+++ src/components/scrollcolor.js
@@ -9,7 +9,8 @@ const ScrollColor = () => {
     });
     const onScroll = () => {
       setState({ x: window.scrollX, y: window.scrollY });
-      // console.log(window.scrollY);
+      //console.log(window.scrollY);
+      //4216
     };
 
     useEffect(() => {
@@ -25,11 +26,17 @@ const ScrollColor = () => {
       <div className="scrollcolor-container">
         <div className="scrollcolor-wrap">
           <div className="scrollcolor-title">Scroll Color</div>
-          <div
-            className="scrollcolor-text"
-            style={{ color: y > 3430 ? "blue" : "black" }}
-          >
-            10 kind hooks
+          <div className="scrollcolor-content-wrap">
+            <div
+              className="scrollcolor-text"
+              style={{ color: y > 3430 ? "blue" : "black" }}
+            >
+              10 kind hooks
+            </div>
+            <div className="scrollcolor-process">스크롤 해보세요</div>
+            <div className="scrollcolor-explain">
+              useState, useEffect, useScroll 훅을 사용하여 만들었습니다.
+            </div>
           </div>
         </div>
       </div>
