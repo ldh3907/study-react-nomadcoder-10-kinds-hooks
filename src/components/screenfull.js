8aac82e0fca3f82ffd40cfc6b70f5fb41487ce6c
ldh3907 <ldh165163@gmail.com>
2021-07-09 15:06:54 +0900

    update : screenfull
    update : screenfull

--- src/components/screenfull.js
+++ src/components/screenfull.js
@@ -22,10 +22,23 @@ const ScreenFul = () => {
     <section id="screenful">
       <div className="screenful-container">
         <div className="screenful-wrap">
-          <div ref={element} className="screenful-content">
-            <img src={image} className="screenful-img" />
-            <button onClick={fulTrigger}>Full Screen</button>
-            <button onClick={exitFulScreen}>Small Screen</button>
+          <div className="screenful-title">Screen Full</div>
+          <div className="screenful-content-wrap">
+            <div ref={element} className="screenful-content">
+              <img src={image} className="screenful-img" />
+              <div className="screenful-button-wrap">
+                <button className="screenful-button" onClick={fulTrigger}>
+                  <div className="screenful-button-text">크게 보기</div>
+                </button>
+                <button className="screenful-button" onClick={exitFulScreen}>
+                  <div className="screenful-button-text">작게 보기</div>
+                </button>
+              </div>
+            </div>
+            <div className="screenful-process">버튼을 눌러주세요</div>
+            <div className="screenful-explain">
+              useRef, useFullScreen 훅을 사용하여 만들었습니다.
+            </div>
           </div>
         </div>
       </div>
